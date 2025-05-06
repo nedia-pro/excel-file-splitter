1049eece49ebedca7a6ae969a2c00684478c18d4
nedia-pro <jemlinedia@gmail.com>
2025-05-06 14:15:43 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,10 +1,4 @@
-import pandas as pd
-import math
-import os
 
-header_row_count = 6
-real_header_row = 4  # الصف 5 في الإكسل = index 4 في بايثون
-max_data_rows = max_total_rows - header_row_count
 
 
 filtered_data = data_rows[data_rows[upc_column_index].astype(str).str.len() >= 12]
@@ -16,12 +10,6 @@ thin_border = Border(
     left=Side(style='thin'), right=Side(style='thin'),
     top=Side(style='thin'), bottom=Side(style='thin')
 )
-center_align = Alignment(horizontal="center", vertical="center", wrap_text=True)
-
-
-    start = i * max_data_rows
-    end = start + max_data_rows
-    chunk = pd.concat([header_rows, filtered_data.iloc[start:end]], ignore_index=True)
 
     filename = f"products_part_{i+1} - الجزء_{i+1}.xlsx"
     filepath = os.path.join(output_folder, filename)
